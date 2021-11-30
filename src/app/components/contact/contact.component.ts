80dfe1a95a2b906f19585e92629b9396c7e6f71f
dcortestorrado <dcortestorrado@gmail.com>
2021-11-29 19:28:34 -0500

    Sweetalert added for CONTACT ME component. 'contact.component.ts' also updated

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Contacto } from 'src/app/models/contacto';
+import { Router } from '@angular/router'; /* nos permite cuadrar la navegación que tiene nuestro usuario después de completar alguna acción */
+import Swal from 'sweetalert2'; /*Aquí importamos Swalert*/
 
 @Component({
   selector: 'app-contact',
@@ -11,7 +14,7 @@ export class ContactComponent implements OnInit {
     contactoForm: FormGroup;
     valorNumerico = /^[0-9]+$/;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router) { /* Acá también hay que declarar router */
       this.contactoForm = this.fb.group({
         nombreContacto: ['', Validators.required],
         correoContacto: ['', [Validators.required, Validators.email]],
@@ -24,7 +27,24 @@ export class ContactComponent implements OnInit {
   }
 
   guardarMensaje(){
-    console.log(this.contactoForm);
+    console.log(this.contactoForm.get('nombreContacto')?.value);
+
+    const CONTACTO: Contacto = {
+
+      nombre: this.contactoForm.get('nombreContacto')?.value,
+      email: this.contactoForm.get('correoContacto')?.value,
+      telefono: this.contactoForm.get('telefonoContacto')?.value,
+      mensaje: this.contactoForm.get('mensajeContacto')?.value
+    }
+
+    console.log(CONTACTO);
+    this.router.navigate(['']);
+    Swal.fire({
+      title: 'Message Sent',
+      text: "The message has been sent. I'll get back to you as soon as possible",
+      icon: 'success',
+      confirmButtonText: 'OK'
+    })
   }
 
 }
